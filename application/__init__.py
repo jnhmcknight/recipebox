5c21a64c76478de2e13ddd94744270f9c2b0723c
Jared McKnight <jmcknight@archertech.ca>
2018-08-28 20:13:40 -0400

    adds simple search capability

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,6 @@
 
 from urlparse import urlparse
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 from application.cache import recipes
 
@@ -30,7 +30,8 @@ def nl2br(string):
 
 @app.route("/")
 def index():
-    return render_template('recipes.html', recipes=recipes.all())
+    title = 'All Recipes'
+    return render_template('recipes.html', recipes=recipes.all(), title=title.decode('utf-8'))
 
 
 @app.route("/recipe/<string:id>")
@@ -49,9 +50,40 @@ def category(id):
     subset = {}
     for recipe in recipes.in_category(id):
         subset.update({recipe: recipes.single(recipe)})
-    return render_template('recipes.html', recipes=subset)
+    title = 'Recipes classified as {}'.format(id)
+    return render_template('recipes.html', recipes=subset, title=title.decode('utf-8'))
 
 
-@app.route("/search")
+@app.route("/search", methods=['GET'])
 def search():
-    pass
+    return render_template('search.html')
+
+@app.route("/search", methods=['POST'])
+def perform_search():
+    if not request.values.get('term'):
+        return redirect(url_for('search'))
+
+    term = request.values.get('term').encode('utf-8').lower()
+
+    recipelist = {}
+    search_type = request.values.get('type', 'ingredient')
+
+    allrecipes = recipes.all()
+    for recipe in allrecipes.keys():
+        if search_type == 'ingredient':
+            for ingredient in allrecipes[recipe].get('ingredients').split('\n'):
+                if term in ingredient.encode('utf-8').lower():
+                    recipelist.update({recipe: allrecipes[recipe]})
+                    break
+
+        elif search_type == 'instruction':
+            for instruction in allrecipes[recipe].get('instructions').split('\n'):
+                if term in instruction.encode('utf-8').lower():
+                    recipelist.update({recipe: allrecipes[recipe]})
+                    break
+
+        elif term in recipe.encode('utf-8').lower():
+            recipelist.update({recipe: allrecipes[recipe]})
+
+    title = 'Found {} Recipes for "{}" in {}s'.format(len(recipelist.keys()), term, search_type)
+    return render_template('recipes.html', recipes=recipelist, title=title.decode('utf-8'))
